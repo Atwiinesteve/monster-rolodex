255b08504661b5f414c7ae35c0b52a92aa53798d
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2023-01-11 13:50:13 +0000

    Added monsters using map()

--- src/App.js
+++ src/App.js
@@ -7,21 +7,43 @@ class App extends Component {
   constructor() {
     super();
 
-    this.state = { name: "Stephen" }
+    // user
+    // this.state = { name: "Stephen" }
+
+    // monsters
+    this.state = {
+      monsters: [
+        {name: "Kiiza"},
+        {name: "Atwiine"},
+        {name: "Stephen"},
+        {name: "JavaScript"},
+        {name: "React"}
+      ]
+    };
+
   }
 
   render() {
 
     return (
 			<div className="App">
-				<header className="App-header">
+
+				{/* <header className="App-header">
 					<p>
 						Welcome { this.state.name } to React Class Components.
 					</p>
 					<button onClick={() => {
             this.setState({ name: "Atwiine "})
           }} >Change Name</button>
-				</header>
+				</header> */}
+
+        {/* Rendering monsters */}
+        { 
+          this.state.monsters.map((monster) => {
+            return <h1 key={ monster._id } >{ monster.name }</h1>
+          })
+        }
+
 			</div>
 		);
 
